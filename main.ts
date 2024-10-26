010efaf94a036497a30085309697f8859d3a95f6
Thomas Haller <hallert60@gmail.com>
2024-10-26 15:26:47 -0500

    implemented modal

--- main.ts
+++ main.ts
@@ -1,98 +1,196 @@
-import { Plugin, Modal, App } from "obsidian";
-const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+import { Plugin, Modal, setIcon, Editor } from 'obsidian';
+
+const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+const LONGEST_WORD = 50;
+const DICTIONARY_DEPTH = 5;
 
 type ApiDefinitions = {
-	definition: string;
+    definition: string;
 };
 
 type ApiMeanings = {
-	partOfSpeech: string;
-	definitions: ApiDefinitions[];
+    partOfSpeech: string;
+    definitions: ApiDefinitions[];
 };
+
 type ApiWord = {
-	meanings: ApiMeanings[];
+    meanings: ApiMeanings[];
 };
+
 type ApiResponse = ApiWord[];
 
 type Definition = {
-	partOfSpeech?: string;
-	definition: string;
+    partOfSpeech?: string;
+    definition: string;
 };
 
+const getCountText = (i: number, size: number) => `${i + 1}/${size}`;
+
+const isValidSelection = (selection: string) =>
+    selection.length > 0 && selection.length < LONGEST_WORD;
+
 const isValidResponse = (resp: unknown) => {
-	return (
-		Array.isArray(resp) &&
-		resp.length > 0 &&
-		"meanings" in resp[0] &&
-		resp[0].meanings.length > 0 &&
-		"definitions" in resp[0].meanings[0] &&
-		resp[0].meanings[0].definitions.length > 0 &&
-		"definition" in resp[0].meanings[0].definitions[0]
-	);
+    return (
+        Array.isArray(resp) &&
+        resp.length > 0 &&
+        'meanings' in resp[0] &&
+        resp[0].meanings.length > 0 &&
+        'definitions' in resp[0].meanings[0] &&
+        resp[0].meanings[0].definitions.length > 0 &&
+        'definition' in resp[0].meanings[0].definitions[0]
+    );
 };
 
 const parseResponse = (resp: ApiResponse): Definition[] => {
-	const res: Definition[] = [];
+    const res: Definition[] = [];
 
-	resp[0].meanings.forEach((meaning) => {
-		res.push({
-			partOfSpeech: meaning.partOfSpeech,
-			definition: meaning.definitions[0].definition,
-		});
-	});
+    for (let i = 0; i < DICTIONARY_DEPTH; i++) {
+        for (let j = 0; j < resp[0].meanings.length; j++) {
+            const meaning: ApiMeanings = resp[0].meanings[j];
 
-	return res;
+            if (i >= meaning.definitions.length) break;
+
+            res.push({
+                partOfSpeech: meaning.partOfSpeech,
+                definition: meaning.definitions[i].definition,
+            });
+        }
+    }
+
+    return res;
 };
 
-const fetchDefinition = async (word: string): Promise<Definition[]> => {
-	try {
-		const res = await fetch(`${API_URL}${word}`);
-		const json = await res.json();
+const fetchDefinitions = async (word: string): Promise<Definition[]> => {
+    try {
+        const res = await fetch(`${API_URL}${word}`);
+        const json = await res.json();
 
-		if (isValidResponse(json)) {
-			return parseResponse(json as ApiResponse);
-		} else {
-			return [{ definition: "No defintion found" }];
-		}
-	} catch (e) {
-		return [{ definition: "An error has occured" }];
-	}
+        if (isValidResponse(json)) {
+            return parseResponse(json as ApiResponse);
+        } else {
+            return [{ definition: 'No defintion found' }];
+        }
+    } catch (e) {
+        return [{ definition: 'An error has occured' }];
+    }
 };
 
-export default class MyPlugin extends Plugin {
-	async onGetDefinition(selection: string) {
-		if (selection.length > 0) {
-			const defs = await fetchDefinition(selection);
+export default class DictionaryHelper extends Plugin {
+    createContent(i: number, defintions: Definition[], modal: Modal) {
+        const contentDiv = modal.contentEl.createDiv();
+        const defintionExists = defintions[i].partOfSpeech !== undefined;
 
-			const modal = new Modal(this.app)
-				.setTitle(selection)
-				.setContent(defs[0].definition);
+        const partOfSpeech = defintionExists
+            ? contentDiv.createSpan({
+                  text: `${defintions[i].partOfSpeech}`,
+                  cls: 'part-of-speech-dictionary',
+              })
+            : null;
 
-			modal.contentEl
-				.createEl("div")
-				.createEl("button", { text: "Prev" }, () => {
-					console.log("Prev");
-				})
-				.createEl("button", { text: "Next" }, () => {
-					console.log("Next");
-				});
-			modal.open();
-		}
-	}
-	async onload() {
-		this.registerEvent(
-			this.app.workspace.on("editor-menu", (menu, editor) => {
-				menu.addItem((item) => {
-					item
-						.setTitle("Get Definition")
-						.setIcon("search")
-						.onClick(
-							async () => await this.onGetDefinition(editor.getSelection())
-						);
-				});
-			})
-		);
-	}
+        const para = contentDiv.createEl('p', {
+            text: `${defintions[0].definition}`,
+            cls: `para-dictionary`,
+        });
 
-	async unload() {}
+        para.toggleClass('center-para-dictionary', !defintionExists);
+
+        const buttonDiv = contentDiv.createDiv({
+            cls: 'modal-button-wrapper-dictionary',
+        });
+
+        let prevButton = null,
+            nextButton = null,
+            span = null;
+
+        if (defintions.length > 1) {
+            prevButton = buttonDiv.createEl('button', {
+                cls: 'modal-button-dictionary',
+            });
+
+            span = buttonDiv.createSpan({
+                text: `${getCountText(i, defintions.length)}`,
+                cls: 'span-count-dictionary',
+            });
+
+            nextButton = buttonDiv.createEl('button', {
+                cls: 'modal-button-dictionary',
+            });
+
+            setIcon(nextButton, 'arrow-right');
+            setIcon(prevButton, 'arrow-left');
+        }
+
+        return { span, para, prevButton, nextButton, partOfSpeech };
+    }
+
+    updateModal(
+        para: HTMLParagraphElement,
+        i: number,
+        definitions: Definition[],
+        span?: HTMLSpanElement | null,
+        part?: HTMLSpanElement | null
+    ) {
+        span?.setText(`${getCountText(i, definitions.length)}`);
+        para.setText(definitions[i].definition);
+        part?.setText(`${definitions[i].partOfSpeech}`);
+    }
+
+    async onGetDefinition(selection: string) {
+        const defs = await fetchDefinitions(selection);
+        let i = 0;
+
+        const modal = new Modal(this.app);
+
+        modal.titleEl.createSpan({
+            text: `${selection}`,
+            cls: 'modal-title-dictionary',
+        });
+
+        const content = this.createContent(i, defs, modal);
+        const { span, para, partOfSpeech } = content;
+
+        content.prevButton?.onClickEvent(() => {
+            i = i > 0 ? i - 1 : i;
+            this.updateModal(para, i, defs, span, partOfSpeech);
+        });
+
+        content.nextButton?.onClickEvent(() => {
+            i = i < defs.length - 1 ? i + 1 : i;
+            this.updateModal(para, i, defs, span, partOfSpeech);
+        });
+
+        modal.open();
+    }
+
+    async onload() {
+        this.registerEvent(
+            this.app.workspace.on('editor-menu', (menu, editor) => {
+                const selection = editor.getSelection();
+                if (isValidSelection(selection)) {
+                    menu.addItem((item) => {
+                        item.setTitle('Get definition')
+                            .setIcon('search')
+                            .onClick(
+                                async () =>
+                                    await this.onGetDefinition(selection)
+                            );
+                    });
+                }
+            })
+        );
+
+        this.addCommand({
+            id: 'get-definition-dictionary',
+            name: 'Get definition',
+            editorCallback: async (editor: Editor) => {
+                const selection = editor.getSelection();
+                if (isValidSelection(selection)) {
+                    await this.onGetDefinition(selection);
+                    return true;
+                }
+
+                return false;
+            },
+        });
+    }
 }
